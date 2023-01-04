66b0382a6c8eb13d6710802454c9b9b394278ced
ikustow <ikustow@yandex.ru>
2023-01-05 00:19:38 +0300

    auth + jwt

---
+++ src/users/users.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { User } from "./users.model";
+import { JwtAuthGuard } from "../auth/jwt-auth.guard";
+
+@ApiTags('Users')
+@Controller('users')
+export class UsersController {
+
+
+  constructor(private usersService: UsersService) {}
+
+  @ApiOperation({summary: 'Create new user in db'})
+  @ApiResponse({status: 200, type: User})
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  create(@Body() userDto: CreateUserDto) {
+    return this.usersService.createUser(userDto);
+  }
+
+  @ApiOperation({summary: 'Get all users from db'})
+  @ApiResponse({status: 200, type: [User]})
+  @Get()
+  getAll() {
+    return this.usersService.getAllUsers();
+  }
+
+}
